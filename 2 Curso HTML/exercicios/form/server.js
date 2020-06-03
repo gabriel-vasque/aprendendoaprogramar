bd15bc01511c29687f63a93e2345e43236a92d6a
Jader Vasque <jader.vasque@gmail.com>
2020-06-03 13:05:01 -0300

    wip
    Co-Authored-By: gabriel-vasque <gabriel-vasque@users.noreply.github.com>

--- 2 Curso HTML/exercicios/form/server.js
+++ 2 Curso HTML/exercicios/form/server.js
@@ -4,9 +4,17 @@ const bodyParser = require('body-parser')
 
 app.use(bodyParser.urlencoded({ extended: true}))
 
-app.post('/usuarios', (req, resp) => {
+// app.get('/usuarios', (req, resp) => {
+    app.post('/usuarios', (req, resp) => {
     console.log(req.body)
-    resp.send('<h1>Parabéns</h1>')
+    console.log(req.query)
+    resp.send('<h1>Parabéns. Usuario Incluido</h1>')
+})
+
+app.post('/usuarios/:id', (req, resp) => {
+    console.log(req.params.id)
+    // console.log(req.body)
+    resp.send('<h1>Parabéns. Usuario Alterado</h1>')
 })
 
 app.listen(3003)

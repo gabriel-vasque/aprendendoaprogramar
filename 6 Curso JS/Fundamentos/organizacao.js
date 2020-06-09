a925feb15b0f62c6b7bd2f0a9e71da4bbc7534ee
Gabriel Vasque <vasque.gabriel@gmail.com>
2020-06-09 00:14:10 -0300

    wip

---
+++ 6 Curso JS/Fundamentos/organizacao.js
@@ -0,0 +1,11 @@
+// sentença de código
+console.log("Sentença de código");
+
+// bloco de código / agupador delimitado pr { }
+// papel do bloco é agupar sentenças de códigos
+{
+    {
+        console.log("Olá");
+        console.log('Mundo!')//Padrão do curso
+    }
+}
---
+++ 6 Curso JS/Fundamentos/variaveisEConstantes.js
@@ -0,0 +1,9 @@
+var a = 3
+
+// forma  mais moderna de declarar variavel
+let b = 4
+
+var a = 30
+b = 40
+
+console.log(a, b)
